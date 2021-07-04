5d99d710b84b4808502fc6fa7352ecbcaa429c84
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-05 03:14:32 +0800

    create user repo

--- repos/users.go
+++ repos/users.go
@@ -1,12 +1,13 @@
 package repos
 
 import (
+	"github.com/aambayec/tut-grpc-go-web/types"
 	"github.com/go-xorm/xorm"
 )
 
 // UsersRepo - the users repo interface
 type UsersRepo interface {
-
+	Create(*types.User) error
 }
 
 // NewUsersRepo - returns a new user's repo
@@ -16,4 +17,16 @@ func NewUsersRepo(db *xorm.Engine) UsersRepo {
 
 type usersRepo struct {
 	db *xorm.Engine
+}
+
+func (u usersRepo) Create(user *types.User) (err error) {
+	if err = types.Validate(user); err != nil {
+		return
+	}
+
+	if _, err = u.db.Insert(user); err != nil {
+		return
+	}
+
+	return
 }
